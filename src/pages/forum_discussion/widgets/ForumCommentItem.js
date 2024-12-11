4810202efe98791b09d82668f2be6377c69e6b50
NguyenDucChien <cnchien@gmail.com>
2024-12-12 02:15:54 +0700

    update: fix bug report comment

--- src/pages/forum_discussion/widgets/ForumCommentItem.js
+++ src/pages/forum_discussion/widgets/ForumCommentItem.js
@@ -453,7 +453,7 @@ const ForumCommentItem = ({ comment, stompClient, user, onCommentDeleted }) => {
             {showCommentOptions && (
               <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10">
                 <div className="py-1" role="menu">
-                {!comment.user.id !== user.id && (
+                {comment?.user?.userId != user?.userId && (
   <button
     className="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100 flex items-center gap-2"
     onClick={() => setShowReportDialog(true)}
